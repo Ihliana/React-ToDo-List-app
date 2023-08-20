f42fc8230c9ecd8211bc3c50425c1fd9a400f900
Ihliana <Ihnatco@protonmail.com>
2023-08-20 19:07:57 +0200

    Update App.js
    Adding comments

--- src/App.js
+++ src/App.js
@@ -8,23 +8,32 @@ function App() {
   const [todo, setTodo] = React.useState("")
   const [todos, setTodos] = React.useState([])
 
-
+  //handle form submission
   const handleSubmit = (e) => {
     e.preventDefault()
 
+    //check if the 'todo' input is not empty
     if(todo !== ""){
+      //a new todo with a unique ID
+      //add the new todo object to the 'todos' array
       setTodos([{id: `${todo}-${Date.now()}`, todo}, ...todos])
     }
 
   }
 
+
+  //function to handle input changes
   const handleInput = (e) => {
+    //update the 'todo' state with the value entered in the input field
     setTodo(e.target.value)
   }
 
 
+  //function to handle todo deletion
   const handleDelete = (id) => {
+    //filter out the todo with the given 'id' from the 'todos' array
     const delTodo = todos.filter((todo) => todo.id !== id)
+    //update the 'todos' state with the filtered array
     setTodos([...delTodo])
 
   }
@@ -38,6 +47,7 @@ function App() {
             <input type='text' 
                     placeholder="Add a new task"
                     onChange={handleInput}
+                    value={todo}
             />
             <button type='submit'>Add</button>
           </form>
