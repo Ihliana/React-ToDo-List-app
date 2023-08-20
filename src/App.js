5ebc2ae1d8ba158d3b93b5bdb2f9b7c7b6b6d01f
Ihliana <Ihnatco@protonmail.com>
2023-08-20 18:42:41 +0200

    Update App.js
    Creating useState Hooks for single todo and a list of todos..to render it on page

--- src/App.js
+++ src/App.js
@@ -4,24 +4,48 @@ import './App.css';
 import Footer from './components/footer'
 
 function App() {
+
+  const [todo, setTodo] = React.useState("")
+  const [todos, setTodos] = React.useState([])
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(todo !== ""){
+      setTodos([{id: `${todo}-${Date.now()}`, todo}, ...todos])
+    }
+
+  }
+
+  const handleInput = (e) => {
+    setTodo(e.target.value)
+  }
+
   return (
     <div className="App">
        <div className="container">
         <h1>Todo List App</h1>
 
-          <form className="todoForm">
+          <form className="todoForm" onSubmit={handleSubmit}>
             <input type='text' 
                     placeholder="Add a new task"
+                    onChange={handleInput}
             />
-            <button>Add</button>
+            <button type='submit'>Add</button>
           </form>
 
           <ul className="todoList">
-            <li className="singleTodo">
-                <span  className="todoText">Learn React</span>
-                <button data-testid="edit-button">Edit</button>
-                <button data-testid='delete-button'>Delete</button>            
-            </li>
+            {
+              todos.map((todo, idx) => (
+                <li className="singleTodo" key={idx}>
+                    <span  className="todoText">{todo.todo}</span>
+                    <button data-testid="edit-button">Edit</button>
+                    <button data-testid='delete-button'>Delete</button>            
+                </li>
+
+              ))
+            }
           </ul>
        </div>
 
