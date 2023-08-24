c5673919ee0b731a09d26d7629239fc473b44883
Ihliana <Ihnatco@protonmail.com>
2023-08-24 12:35:21 +0200

    adding more tests for form
    adding interaction tests

--- src/components/TodoForm.test.js
+++ src/components/TodoForm.test.js
@@ -2,7 +2,7 @@
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom';
 //to test user interactions 
-import user from '@testing-library/user-event'
+import userEvent from '@testing-library/user-event'
 
 import TodoForm from './TodoForm';
 
@@ -18,5 +18,45 @@ describe("testing TodoForm component", () => {
 
         expect(inputElement).toBeInTheDocument()
         expect(buttonElement).toBeInTheDocument()
+    })
+
+
+    test('input captures user input', async ()=> {
+      render(<TodoForm />)
+
+        const inputElement = screen.getByPlaceholderText("Add a new task")
+
+        /* simulate a user adding a new task*/
+        await userEvent.type(inputElement, 'New task text')
+
+        expect(inputElement).toHaveValue('New task text')
+    })
+
+    test('button text changes based on editId', () => {
+        render(<TodoForm editId={123} />)
+
+        const editBtn = screen.getByRole("button", {
+            name: /edit/i
+        })
+
+        expect(editBtn).toBeInTheDocument()
+    })
+
+    test('calls handleSubmit when form is submitted', async () => {
+        const handleSubmit = jest.fn()
+        render(<TodoForm handleSubmit={handleSubmit} />)
+
+        const inputElement = screen.getByPlaceholderText("Add a new task");
+        const buttonElement = screen.getByRole("button", {
+            name: /add/i 
+        })
+
+        await userEvent.type(inputElement, "New task text")
+        await userEvent.click(buttonElement)
+
+        expect(handleSubmit).toHaveBeenCalledTimes(1)
+
+
+
     })
 })
